3431a352ccac53aabb2367ace11e8bf59cd7074a
Jan Jorgensen <jandjorgensen@gmail.com>
2016-03-28 18:49:25 -0700

    0.0.1

---
+++ dist/cjs/bubble.js
@@ -0,0 +1,17 @@
+/*can-map-lazy@0.0.0#bubble*/
+var can = require('can/util/util');
+require('can/map/bubble');
+var bubble = can.bubble;
+module.exports = can.extend({}, bubble, {
+    childrenOf: function (parentMap, eventName) {
+        if (parentMap._nestedReference) {
+            parentMap._nestedReference.each(function (child, ref) {
+                if (child && child.bind) {
+                    bubble.toParent(child, parentMap, ref(), eventName);
+                }
+            });
+        } else {
+            bubble._each.apply(this, arguments);
+        }
+    }
+});
